b0ec4bfee64bab4bacce7f52d6d0fc1c7104fb22
livefromblessings-pc <blessing.sokoya@colorado.edu>
2019-01-29 13:01:49 -0700

    finished final  problem from Lab 02

---
+++ Labs/Lab01/Lab01FinalProblem.sh
@@ -0,0 +1,3 @@
+cut -f 2-4 -d , PredPreyData.csv >> EditPredPreyData.csv | tail EditPredPreyData.csv
+head -1 EditPredPredData.csv >> Last11PredPreyData.csv
+tail -11 EditPredPreyData.csv >> Last11PredPreyData.csv

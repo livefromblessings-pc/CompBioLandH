0439aaa9c47002efbedc52d565c6c93e5ff03a2e
livefromblessings-pc <blessing.sokoya@colorado.edu>
2019-01-29 17:03:35 -0700

    fixed final problem script and folder

--- Labs/Lab01/Lab01FinalProblem.sh
+++ Labs/Lab01/Lab01FinalProblem.sh
@@ -1,3 +1,8 @@
-cut -f 2-4 -d , PredPreyData.csv >> EditPredPreyData.csv | tail EditPredPreyData.csv
-head -1 EditPredPredData.csv >> Last11PredPreyData.csv
+#Remove first empty column from file, then view the last lines of new edited file
+cut -f 2-4 -d , PredPreyData.csv >> EditPredPreyData.csv
+
+#Extract first line from edited file and make new file
+head -1 EditPredPreyData.csv >> Last11PredPreyData.csv
+
+#Extract last 11 lines from edited file and add to same new file
 tail -11 EditPredPreyData.csv >> Last11PredPreyData.csv
